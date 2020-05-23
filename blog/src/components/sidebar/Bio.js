5385eaff86ff6ab2029c6c185b0c820078e94393
Shakti Misra <shakti.misra@sap.com>
2020-05-23 20:43:25 +0530

    Adding the initial version of the blog

---
+++ blog/src/components/sidebar/Bio.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "./sidebar.css"
+
+import logo from "../../images/logo.png"
+
+const Bio = ({ author, tagline }) => {
+
+  return (
+    <div className="bio-main w-75">
+      <img src={logo} style={{ maxWidth: `100px` }} className="profile-img" alt=""/>
+      <h3 className="mt-2 author-bio">{author}</h3>
+      <small className="text-muted">{tagline}</small>
+    </div>
+  )
+}
+
+export default Bio
